43072811854bc152e532afc43e38606f9f595ca1
davidmbio <mbio.david@gmail.com>
2013-12-02 23:13:13 -0600

    Retoques finales

--- src/Public/views/Admin.php
+++ src/Public/views/Admin.php
@@ -1,61 +1,67 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SanViernes- AdminLogin -</title>
-    <link rel="stylesheet" href="../../../web/css/login-admin.css" type="text/css" />
-    <script src="../../../web/js/jquery.js"/>
-    
-    <script language="javascript" type="text/javascript">	
+<?php
+include '../Controller_master.php';
+$Contenido= Regresa_Logeo();
+
+require '../templates/tamplates.php';
+
+$temp= new Template('../layout/layout.tpl');
+$temp->set('title','Logeo a Licoreria-Admin ');
+$temp->set('header', 'Acceso a SanViernes');
+$temp->set('Tipo_Sesion', $Contenido);
+$temp->set('content','
+<form action="javascript: Logear();" method="post" id="FLogin" class="modal fade in" >  
+    <div class="alert alert-info"><h4>Acceso a Sanviernes - Admin</h4></div>
+    <ul style="padding-right: 40px; text-align: center;">                
+            <input name="tipo" type="hidden" id="tipo" value="login"/>
+
+<div class="row" style="padding-bottom: 5px;">
+
+            <img class="img-rounded" src="../../../web/img/key.png" /> </td>
+        </div>
+        <input name="user" type="text" id="user" placeholder="Nombre de usuario" class="required text input" />
+            <br><input name="pass" type="password" id="pass" placeholder="Contrase&ntilde;a" class="required input" />        </td>
+                 
+      </ul>
+    <div class="modal-footer">
+        <input name="agregar" type="submit" id="agregar" class="btn btn-primary" value="Logear" />
+        <input name="cancelar" type="button" id="cancelar" class="btn" value="Cancelar" onclick="Cancelado();" />
+    </div>
+</form>  
+');
+$temp->set('En_Promocion', En_Promocion());
+echo $temp->output();
+?>
+
+
+<script language="javascript" type="text/javascript">
 	function Logear(){
 		var str = $("#FLogin").serialize();
 		$.ajax({
-			url: '../model/Accesso.php',
+			url: '../model/admin/Logeo.php',
 			data: str,
-			type: 'POST',
+			type: 'post',
 			success: function(data){
-                            if(data != ""){
-                                if(data==1)
-                                    Accedido();
-                                else
-                                    alert(data);
-                                }
+				if(data != "")
+                                    {
+                                        if(data==1)
+                                            Accedido();
+                                        else
+                                            alert(data);
+                                    }
+                                //Accedido();
 			}
 		});
 	};
         
         function Accedido() {
+            location.href='../../Admin/View/index.php';
+        } 
+        function Cancelado() {
             location.href='index.php';
         } 
-        setTimeout ("Accedido()", 20000);
-    </script>
+</script>
 
-
-</head>
-<body class="login">
-    <div class="loginbox radius">
-    <h2 style="color:#FFF; text-align:center">Licoreria San-Viernes</h2>
-            <div class="loginboxinner radius">        
-            <div class="loginform">
-                    <form method="post" id="FLogin" >
-                    <p>
-                        <label for="username" class="bebas">Usuario:</label>
-                        <input type="text" id="username" name="username" value="" class="radius2" />
-                    </p>
-                    <p>
-                        <label for="password" class="bebas">Contrase&ntilde;a</label>
-                        <input type="password" id="password" name="password"  class="radius2" />
-                    </p>
-                    <p>
-                        <button class="radius title" name="client_login">Login</button>
-                    </p>
-                    <p>
-                        <button class="radius title" name="client_login">Cancel</button>
-                    </p>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+<script language="javascript" type="text/javascript" src="../../../../web/js/lib/jquery-1.3.2.min.js"></script>
+<script language="javascript" type="text/javascript" src="../../../../web/js/lib/jquery.blockUI.js"></script>
+<script language="javascript" type="text/javascript" src="../../../../web/js/lib/jquery.validate.1.5.2.js"></script>
+    
--- src/Public/views/Upload.php
+++ src/Public/views/Upload.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 include '../Controller_master.php';
 
